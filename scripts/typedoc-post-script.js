038fffd9ea5c3b20fd7132d5e0b0cc63d0fe232c
Nicolae Lupei <nicolae.lupei@qodalis.com>
2025-01-10 10:55:29 +0200

    fix: missing CNAME file, script to change the workspace version

--- scripts/typedoc-post-script.js
+++ scripts/typedoc-post-script.js
@@ -3,6 +3,7 @@ const path = require("path");
 
 const OUTPUT_DIR = "docs";
 const CNAME_FILE = "CNAME";
+const CNAME_LOCATION = "assets/github/" + CNAME_FILE;
 
-fs.copyFileSync(CNAME_FILE, path.join(OUTPUT_DIR, CNAME_FILE));
+fs.copyFileSync(CNAME_LOCATION, path.join(OUTPUT_DIR, CNAME_FILE));
 console.log("CNAME file restored.");
