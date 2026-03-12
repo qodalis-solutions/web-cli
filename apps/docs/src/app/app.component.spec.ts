d0e6341fab606ca54a75e7713e21385899f25d61
Nicolae Lupei <nicolae.lupei@qodalis.com>
2026-03-13 01:28:33 +0200

    fix(docs): add TranslateModule to test bed

--- apps/docs/src/app/app.component.spec.ts
+++ apps/docs/src/app/app.component.spec.ts
@@ -1,5 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { TranslateModule } from '@ngx-translate/core';
 import { AppComponent } from './app.component';
 import { CliModule } from '@qodalis/angular-cli';
 
@@ -7,7 +8,7 @@ describe('AppComponent', () => {
     beforeEach(() =>
         TestBed.configureTestingModule({
             declarations: [AppComponent],
-            imports: [CliModule, RouterTestingModule],
+            imports: [CliModule, RouterTestingModule, TranslateModule.forRoot()],
         }),
     );
 
