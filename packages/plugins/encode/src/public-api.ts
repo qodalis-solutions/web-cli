7932c0bc56c20d905a5f3d8aad7cf74c8ac5f93c
Nicolae Lupei <nicolae.lupei@qodalis.com>
2026-03-18 21:48:16 +0200

    feat(encode): wire up encodeModule with all 8 processors

--- packages/plugins/encode/src/public-api.ts
+++ packages/plugins/encode/src/public-api.ts
@@ -2,15 +2,30 @@
  * Public API Surface of encode
  */
 
-export * from './lib/processors/cli-encode-command-processor';
-export * from './lib/version';
+export * from './lib';
 
 import { ICliModule } from '@qodalis/cli-core';
-import { CliCliEncodeCommandProcessor } from './lib/processors/cli-encode-command-processor';
+import { CliBase64CommandProcessor } from './lib/processors/cli-base64-command-processor';
+import { CliHexCommandProcessor } from './lib/processors/cli-hex-command-processor';
+import { CliUrlCommandProcessor } from './lib/processors/cli-url-command-processor';
+import { CliHashCommandProcessor } from './lib/processors/cli-hash-command-processor';
+import { CliJwtCommandProcessor } from './lib/processors/cli-jwt-command-processor';
+import { CliBinaryCommandProcessor } from './lib/processors/cli-binary-command-processor';
+import { CliRotCommandProcessor } from './lib/processors/cli-rot-command-processor';
+import { CliMorseCommandProcessor } from './lib/processors/cli-morse-command-processor';
 import { API_VERSION } from './lib/version';
 
 export const encodeModule: ICliModule = {
     apiVersion: API_VERSION,
     name: '@qodalis/cli-encode',
-    processors: [new CliCliEncodeCommandProcessor()],
+    processors: [
+        new CliBase64CommandProcessor(),
+        new CliHexCommandProcessor(),
+        new CliUrlCommandProcessor(),
+        new CliHashCommandProcessor(),
+        new CliJwtCommandProcessor(),
+        new CliBinaryCommandProcessor(),
+        new CliRotCommandProcessor(),
+        new CliMorseCommandProcessor(),
+    ],
 };
