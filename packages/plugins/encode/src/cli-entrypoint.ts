20e3daead55a4eb682b2ff0ff5369fe89fd31f12
Nicolae Lupei <nicolae.lupei@qodalis.com>
2026-03-18 21:48:16 +0200

    feat(encode): wire up encodeModule with all 8 processors

--- packages/plugins/encode/src/cli-entrypoint.ts
+++ packages/plugins/encode/src/cli-entrypoint.ts
@@ -1,11 +1,29 @@
 import { bootCliModule, ICliModule } from '@qodalis/cli-core';
-import { CliCliEncodeCommandProcessor } from './lib/processors/cli-encode-command-processor';
+import {
+    CliBase64CommandProcessor,
+    CliHexCommandProcessor,
+    CliUrlCommandProcessor,
+    CliHashCommandProcessor,
+    CliJwtCommandProcessor,
+    CliBinaryCommandProcessor,
+    CliRotCommandProcessor,
+    CliMorseCommandProcessor,
+} from './lib';
 import { API_VERSION } from './lib/version';
 
 const module: ICliModule = {
     apiVersion: API_VERSION,
     name: '@qodalis/cli-encode',
-    processors: [new CliCliEncodeCommandProcessor()],
+    processors: [
+        new CliBase64CommandProcessor(),
+        new CliHexCommandProcessor(),
+        new CliUrlCommandProcessor(),
+        new CliHashCommandProcessor(),
+        new CliJwtCommandProcessor(),
+        new CliBinaryCommandProcessor(),
+        new CliRotCommandProcessor(),
+        new CliMorseCommandProcessor(),
+    ],
 };
 
 bootCliModule(module);
